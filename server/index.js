3849ca0903a1132b71b75bbf4f5be10b06ff3ed7
Matthew Baya <chiptiep99@gmail.com>
2024-05-28 23:41:51 +0800

    feat: add user schema

--- server/index.js
+++ server/index.js
@@ -78,17 +78,11 @@ const resolvers = {
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 startStandaloneServer(server, {
-  listen: { port: 4000 },
+  listen: { port: 3000 },
 }).then(({ url }) => [console.log(`🚀  Server ready at: ${url}`)]);
