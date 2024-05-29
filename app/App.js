0fbd33b67d3b2e16715722a826815a0851c01a8a
Matthew Baya <chiptiep99@gmail.com>
2024-05-29 19:04:00 +0800

    feat: app login screen

--- app/App.js
+++ app/App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>Line 2</Text>
+      </View>
+      <Image
+        source={{ uri: "https://reactjs.org/logo-og.png" }}
+        style={{
+          width: 100,
+          height: 100,
+          objectFit: "cover",
+        }}
+      />
+      <View style={{ marginVertical: 30 }}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(e) => console.log(e)}
+          value={""}
+          placeholder="Email"
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(e) => console.log(e)}
+          value={""}
+          placeholder="Password"
+        />
+      </View>
+      <Button
+        onPress={() => {
+          console.log("You tapped the button!");
+        }}
+        title="Login"
+      />
     </View>
   );
 }
@@ -13,8 +44,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#900",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    margin: 5,
+
+    borderWidth: 1,
+    padding: 10,
   },
 });
