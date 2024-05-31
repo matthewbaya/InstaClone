2fa172cc420d81ba63673687162bb102b35dd7f6
Matthew Baya <chiptiep99@gmail.com>
2024-05-31 23:57:18 +0800

    fix : server follow model fix

--- server/models/Follow.js
+++ server/models/Follow.js
@@ -27,5 +27,71 @@ class Follow {
     const findOne = this.findById(result.insertedId);
     return findOne;
   }
+  static async findFollowing(_id) {
+    const followCollection = this.collection();
+    const data = await followCollection
+      .aggregate([
+        {
+          $match: {
+            followerId: new ObjectId(_id),
+          },
+        },
+        {
+          $lookup: {
+            from: "Users",
+            localField: "followingId",
+            foreignField: "_id",
+            as: "following",
+          },
+        },
+        {
+          $unwind: {
+            path: "$following",
+          },
+        },
+        {
+          $project: {
+            "following.password": 0,
+          },
+        },
+      ])
+      .toArray();
+
+    return data;
+  }
+
+  static async findFollower(_id) {
+    const followCollection = this.collection();
+
+    const data = await followCollection
+      .aggregate([
+        {
+          $match: {
+            followingId: new ObjectId(_id),
+          },
+        },
+        {
+          $lookup: {
+            from: "Users",
+            localField: "followerId",
+            foreignField: "_id",
+            as: "follower",
+          },
+        },
+        {
+          $unwind: {
+            path: "$follower",
+          },
+        },
+        {
+          $project: {
+            "follower.password": 0,
+          },
+        },
+      ])
+      .toArray();
+    console.log(data);
+    return data;
+  }
 }
 module.exports = Follow;
